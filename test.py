6a088849d0b6db1c5e71d26d43c03afdb7092f27
steven <wtan12@jhu.edu>
2019-07-04 04:58:18 +0100

    add button and GPIO input module

--- test.py
+++ test.py
@@ -2,16 +2,16 @@
 import RPi.GPIO as GPIO
 import sys
 import time
-LED = 21
-Button = 5
+LED = 22
+Button = 12
 def main():
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(Button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(LED,GPIO.OUT)
     GPIO.add_event_detect(Button, GPIO.BOTH, callback = detect, bouncetime = 200)
     while (True):
-        #pStatus(Button)
+        pStatus(Button)
         GPIO.output(LED,True)
         time.sleep(0.5)
         GPIO.output(LED,False)        
@@ -26,11 +26,19 @@ def pStatus(channel):
 def detect(chn):
     if (GPIO.input(Button)):
         print("Button is pressed")
+        
         destroy()
+        
+        for i in range(5):
+            GPIO.output(LED, False)
+            time.sleep(1)
+        #destroy()
     else: 
         #do nothing
         print("Button is not pressed")
 def destroy():
-    GPIO.output(LED, GPIO.LOW)
-    GOPI.cleanup()
+    GPIO.output(LED, GPIO.HIGH)
+    GPIO.cleanup()
+    quit()
+
 main()
